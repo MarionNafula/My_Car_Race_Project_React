b8072003c28a3b4e11ace10cb376aac4425b3d1e
Marion <wekesamarion100@gmail.com>
2025-01-30 23:05:07 +0300

    created circuit page

--- src/pages/Circuits.jsx
+++ src/pages/Circuits.jsx
@@ -2,3 +2,21 @@ import CircuitForm from "../components/CircuitForm";
 import CircuitList from "../components/CircuitList";
 import { getCircuits } from "../api";
 import { useState, useEffect } from "react";
+
+const Circuits = () => {
+  const [circuits, setCircuits] = useState([]);
+
+  useEffect(() => {
+    getCircuits().then(setCircuits);
+  }, []);
+
+  return (
+    <div>
+      <h1>Circuits</h1>
+      <CircuitForm refreshCircuits={() => getCircuits().then(setCircuits)} />
+      <CircuitList />
+    </div>
+  );
+};
+
+export default Circuits;
