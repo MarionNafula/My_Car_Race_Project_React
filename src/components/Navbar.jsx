29a573956d62ff7c9409807233a9939e3d74baf8
Marion <wekesamarion100@gmail.com>
2025-01-30 22:13:39 +0300

    created Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,16 @@
 import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/drivers">Drivers</Link></li>
+          <li><Link to="/circuits">Circuits</Link></li>
+          <li><Link to="/stats">Stats</Link></li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
