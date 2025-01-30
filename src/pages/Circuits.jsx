92fd449844a81ee17c052f9775d62cad7caea4de
Marion <wekesamarion100@gmail.com>
2025-01-30 23:03:46 +0300

    imported

---
+++ src/pages/Circuits.jsx
@@ -0,0 +1,4 @@
+import CircuitForm from "../components/CircuitForm";
+import CircuitList from "../components/CircuitList";
+import { getCircuits } from "../api";
+import { useState, useEffect } from "react";
