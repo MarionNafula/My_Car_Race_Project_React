f1f4813bfa48f601471a3825d129dea6f0ac378f
Marion <wekesamarion100@gmail.com>
2025-01-30 22:43:30 +0300

    creating routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,19 @@ import Home from "./pages/Home";
 import Drivers from "./pages/Drivers";
 import Circuits from "./pages/Circuits";
 import Stats from "./pages/Stats";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/drivers" element={<Drivers />} />
+        <Route path="/circuits" element={<Circuits />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
