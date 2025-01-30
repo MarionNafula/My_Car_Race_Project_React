96296a98131b8d362864e266bb5cc5df48ae756b
Marion <wekesamarion100@gmail.com>
2025-01-30 22:37:29 +0300

    created driver page

--- src/pages/ Drivers.jsx
+++ src/pages/ Drivers.jsx
@@ -1,3 +1,25 @@
 import { useEffect, useState } from "react";
 import { getDrivers } from "../api";
 import DriverForm from "../components/DriverForm";
+
+const Drivers = () => {
+    const [drivers, setDrivers] = useState([]);
+  
+    useEffect(() => {
+      getDrivers().then(setDrivers);
+    }, []);
+  
+    return (
+      <div>
+        <h1>Drivers</h1>
+        <DriverForm refreshDrivers={() => getDrivers().then(setDrivers)} />
+        <ul>
+          {drivers.map(driver => (
+            <li key={driver.id}>{driver.name} - {driver.team}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Drivers;
