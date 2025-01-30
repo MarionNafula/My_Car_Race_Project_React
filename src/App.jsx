a5d14f6a9b452ba2ae7048630218a8f1fbd0afdc
Marion <wekesamarion100@gmail.com>
2025-01-30 22:40:58 +0300

    imports

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Drivers from "./pages/Drivers";
+import Circuits from "./pages/Circuits";
+import Stats from "./pages/Stats";
