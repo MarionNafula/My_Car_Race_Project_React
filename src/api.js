aaa9f46a59f83a3fbe563e0ab073a48961a2be10
Marion <wekesamarion100@gmail.com>
2025-01-30 23:09:43 +0300

    exported circuit

--- src/api.js
+++ src/api.js
@@ -1,16 +1,29 @@
 const BASE_URL = "http://127.0.0.1:5000";
 
 export const getDrivers = async () => {
-    const response = await fetch(`${BASE_URL}/drivers`);
-    return response.json();
-  };
+  const response = await fetch(`${BASE_URL}/drivers`);
+  return response.json();
+};
 
-  export const addDriver = async (driver) => {
-    const response = await fetch(`${BASE_URL}/drivers`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(driver),
-    });
-    return response.json();
-  };
+export const addDriver = async (driver) => {
+  const response = await fetch(`${BASE_URL}/drivers`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(driver),
+  });
+  return response.json();
+};
 
+export const getCircuits = async () => {
+  const response = await fetch(`${BASE_URL}/circuits`);
+  return response.json();
+};
+
+export const addCircuit = async (circuit) => {
+  const response = await fetch(`${BASE_URL}/circuits`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(circuit),
+  });
+  return response.json();
+};
