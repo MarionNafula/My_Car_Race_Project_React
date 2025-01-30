e3e6d9ced68c12f0db3af75a54b86730559310cc
Marion <wekesamarion100@gmail.com>
2025-01-30 23:06:20 +0300

    created stats page

---
+++ src/pages/Stats.jsx
@@ -0,0 +1,10 @@
+const Stats = () => {
+    return (
+      <div>
+        <h1>Race Stats</h1>
+        <p>Coming soon: View race statistics, lap times, and rankings!</p>
+      </div>
+    );
+  };
+  
+  export default Stats;
