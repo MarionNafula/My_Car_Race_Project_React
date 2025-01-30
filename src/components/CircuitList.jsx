0870058a0d62c0eb528e1879eceaf1b00558d101
Marion <wekesamarion100@gmail.com>
2025-01-30 23:02:01 +0300

    created circuit list

--- src/components/CircuitList.jsx
+++ src/components/CircuitList.jsx
@@ -1,3 +1,23 @@
 import { useEffect, useState } from "react";
 import { getCircuits } from "../api";
 
+const CircuitList = () => {
+  const [circuits, setCircuits] = useState([]);
+
+  useEffect(() => {
+    getCircuits().then(setCircuits);
+  }, []);
+
+  return (
+    <div>
+      <h2>Circuits</h2>
+      <ul>
+        {circuits.map(circuit => (
+          <li key={circuit.id}>{circuit.name} - {circuit.location} ({circuit.length} km)</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CircuitList;
