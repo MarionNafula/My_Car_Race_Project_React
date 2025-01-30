fa79025015b4c2015a0906e040122eb877aef50d
Marion <wekesamarion100@gmail.com>
2025-01-30 23:07:34 +0300

    created home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to Car Race Central</h1>
+        <p>Manage race circuits, drivers, and stats with ease!</p>
+        <img src="/assets/race-track.jpg" alt="Race track" />
+      </div>
+    );
+  };
+  
+  export default Home;
