5e3fc8817b1ec51f42f61d9e7f3b4ae5d54ad0ff
Marion <wekesamarion100@gmail.com>
2025-01-31 15:04:55 +0300

    added vite.config file

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 5173, // Change the port if needed
+    open: true, // Automatically opens the browser
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000', // Flask backend URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
